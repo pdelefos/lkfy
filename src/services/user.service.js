1184a678275f22360ee34c1eac6e6d19f9d884e2
pdelefos <pdelefos@pdelefoss-MacBook-Pro.local>
2018-02-08 22:22:58 +0100

    removed service and validation

--- src/services/user.service.js
+++
@@ -1,8 +0,0 @@
-import UserModel from "../models/user.model"
-
-function createUser(userData) {
-  let newUser = new UserModel(userData)
-  return newUser.save()
-}
-
-module.exports = { createUser: createUser }
